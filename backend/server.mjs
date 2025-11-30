96b03c31b698ebae1de1016f92d4c71939bae4c0
Nicholas Nguyen <nxcholas4652@gmail.com>
2025-11-30 15:27:56 -0800

    preparing for deployment

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,19 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import TCGdex from '@tcgdex/sdk';
 import { connectDB } from "./config/db.mjs";
 import userRouter from "./routes/userRoutes.mjs";
 import setRouter from "./routes/setRoutes.mjs";
+import path from "path";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-if(process.env.node_ENV !== "production") {
+if (process.env.node_ENV !== "production") {
   //  cors
   app.use(
     cors({
@@ -23,12 +24,20 @@ if(process.env.node_ENV !== "production") {
 }
 
 // middleware - routes
-app.use("", userRouter)
-app.use("", setRouter)
+app.use("", userRouter);
+app.use("", setRouter);
 
+// middleware for deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "/dist", "index.html"));
+  });
+}
 
 connectDB().then(
   app.listen(PORT, () => {
     console.log(`server started on PORT ${PORT}`);
   })
-)
+);
